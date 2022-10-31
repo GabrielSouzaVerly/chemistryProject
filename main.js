5b37cba567b20bc41722d265256aa82dfe89785b
your Name <you@example.com>
2022-10-31 17:59:34 -0300

    Início das alterações

---
+++ main.js
@@ -0,0 +1,6 @@
+const canvas = document.querySelector("canvas")
+const ctx = canvas.getContext("2d")
+canvas = window.innerWidth
+canvas = window.innerHeight
+ctx.fillStyle = red
+ctx.fillRect(0,0,100,200)
